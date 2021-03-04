bedf96848c7ad8ac46147d9f73ec4036a893f2b8
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-04 07:28:34 -0600

    Distancia entre dos puntos (Fundamentos)

--- main.cpp
+++ main.cpp
@@ -1,74 +1,22 @@
 #include <iostream>
-#include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
 int main(void){
-    printf("Hello world!\n\n\n\n\n");
+//    Codifica un programa en C++ (con cin y cout) que calcule la distancia
+//    entre dos puntos en un plano cartesiano
 
-    cout<< "Hello world!"
-        << endl << 5
-        << endl << "Una mas debajo\n\n\n\n"
-        << "La ultima al lado de otra\n\n\n\n"
-        << endl << endl << endl << endl ;
+    double x1,y1,x2,y2,d;
 
-    system("pause");
-    system("cls");
+    cout<<"Dame x1 ";cin>>x1;
+    cout<<"Dame y1 ";cin>>y1;
+    cout<<"Dame x2 ";cin>>x2;
+    cout<<"Dame y2 ";cin>>y2;
 
+    d = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
 
-    int a = 5;
-    float b = 6.7;
-    char c='@';
-
-    cout<< a<<endl
-        << 3 <<endl
-        << b <<endl
-        << c
-        << endl << endl << endl << endl ;
-
-    cout<< "a="<<a<<endl
-        << 3 <<endl
-        << "b="<<b <<endl
-        << "c="<<c
-        << endl << endl << endl << endl ;
-
-    system("pause");
-    system("cls");
-
-    cout<<"Dame un numero ";
-    cin>>a;
-    cout<<"Dame otro numero ";
-    cin>>b;
-    cout<<"Dame un simbolo ";
-    cin>>c;
-
-    cout<< "a="<<a<<endl
-        << "b="<<b <<endl
-        << "c="<<c <<endl;
-
-    system("pause");
-    system("cls");
-
-    float temperatura;
-    cout<<"Dame un entero, un char y un flotante ";
-    cin>>a>>c>>temperatura;
-
-    cout<< "a= " <<a <<endl
-        << "c= " <<c <<endl
-        << "b= " <<temperatura <<endl;
-
-    system("pause");
-    system("cls");
-
-    char Vector[10];
-    for(int i=0;i<10;i++){
-        cout<< "Ingresa Vector["<<i<<"] ";
-        cin>>Vector[i];
-    }
-
-    for(int j=0;j<10;j++){
-        cout<< Vector[j]<<endl;
-    }
+    cout << "d = " << d << endl;
 
     return 0;
 }
