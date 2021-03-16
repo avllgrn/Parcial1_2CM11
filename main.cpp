87254d0969ea06338fe46762afc21467784d1547
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-16 07:58:27 -0600

    Primeros metodos de string

--- main.cpp
+++ main.cpp
@@ -6,24 +6,37 @@
 using namespace std;
 
 int main(void){
-    srand(time(NULL));
 
-    Punto V, V2;
-    int i, n;
+    string cadena1, cadena2("Cadena dos"), enter;
 
-    V.modificaTusDatos(rand() % 100, rand() % 100);
-
-
-    ofstream ArchivoDeSalida("Puntos.txt");
-    V.guardaTusDatos(ArchivoDeSalida);
-    ArchivoDeSalida.close();
+    cout<<"cadena1: "<<cadena1<<endl;
+    cout<<"cadena2: "<<cadena2<<endl;
 
 
-    ifstream ArchivoDeEntrada("Puntos.txt");
-    V2.cargaTusDatos(ArchivoDeEntrada);
-    ArchivoDeEntrada.close();
+    cout<<"cadena1 tiene "<<cadena1.size()<<" caracteres."<<endl;
+    cout<<"cadena2 tiene "<<cadena2.size()<<" caracteres."<<endl;
 
-    V2.muestraTusDatos();
+    cout<<"cadena1 tiene "<<cadena1.length()<<" caracteres."<<endl;
+    cout<<"cadena2 tiene "<<cadena2.length()<<" caracteres."<<endl;
+
+    cout<<"cadena1 esta vacia? "<<cadena1.empty()<<endl;
+    cout<<"cadena2 esta vacia? "<<cadena2.empty()<<endl;
+
+
+    cout<<"Ingresa solo un nombre ";
+    cin>>cadena1;
+    getline(cin,enter);
+    cout<<"Ingresa tus apellidos ";
+    getline(cin,cadena2);
+    cout<<"Hola, "<<cadena1<<" "<<cadena2<< "! =)"<<endl;
+
+
+    cadena1.clear();
+    cadena2.clear();
+    cout<<"cadena1: "<<cadena1<<endl;
+    cout<<"cadena2: "<<cadena2<<endl;
+
+
 
     return 0;
 }
