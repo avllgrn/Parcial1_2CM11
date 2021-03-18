d3889f906b08e2a5cb0adad5e9ebb57d1873510f
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-18 07:55:33 -0600

    Concatenar dos cadenas, en una tercera

--- main.cpp
+++ main.cpp
@@ -3,57 +3,21 @@ using namespace std;
 
 int main(void){
 
-    string cadena, cadena2;
+    string cadena, cadena2, cadena3;
 
     int i, n;
 
     cout<<"Ingresa una cadena ";
     getline(cin, cadena);
-    n = cadena.size();
+    cout<<"Ingresa otra cadena ";
+    getline(cin, cadena2);
+
+    cadena3.append(cadena);
+    cadena3.append(cadena2);
 
     cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena);
-    cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena);
-    cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena);
-    cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena);
-    cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena);
-    cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena);
-    cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena);
-    cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena);
-    cout<<"Tu cadena: "<<cadena<<endl;
-    cout<<"cadena2  : "<<cadena2<<endl<<endl;
-    system("pause");
+    cout<<"Tu cadena2: "<<cadena2<<endl;
+    cout<<"cadena3  : "<<cadena3<<endl<<endl;
 
     return 0;
 }
